0711f781e61e2da66ebac17f0483d5930436f93e
IvyNerd <hames.shawn08@gmail.com>
2026-03-09 16:30:05 -0400

    Update models.py

--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class Customer:
         return self.customer_id, self.first_name, self.last_name, self.phone_number
     
     def get_overdue_books(self):
-     return database.get_overdue_books_by_customer(self.customer_id)
+        return database.get_overdue_books_by_customer(self.customer_id)
     
 class Library:
     def __init__(self):
@@ -59,44 +59,58 @@ class Library:
 
 
     #Add Customer
-
-
+    def add_customer (self, first_name, last_name, phone):
+        database.create_customer(first_name, last_name, phone)
 
     #Remove Customer
-
-
+    def remove_customer (self, customer_id):
+        database.delete_customer(customer_id)
 
     #Update Customer
+    def update_customer (self, first_name, last_name, phone, customer_id):
+        database.update_customer(first_name, last_name, phone, customer_id)
 
 
 
     #Search Book
-
-
+    def search_book(self, title=None, book_id=None, author=None):
+        return database.search_book(title=title, book_id=book_id, author=author)
 
     # Get all books
-
-
+    def get_all_books(self):
+        return database.get_all_books()
 
     #Get all Customers
-
-
+    def get_all_customers(self):
+        return database.get_all_customers()
 
     #checkout book
-
-
-
+    def checkout_book (self, book_id, customer_id, checkout_date, due_date):
+        database.log_checkout(book_id, customer_id, checkout_date, due_date)
+        existing_book = self.books_by_id[book_id]
+        existing_book.available_copies = existing_book.available_copies - 1
 
 
     #Return Book
-
-
+    def return_book (self, checkout_id, return_date, book_id):
+        database.log_return(checkout_id, return_date)
+        existing_book = self.books_by_id[book_id]
+        existing_book.available_copies = existing_book.available_copies + 1
 
 
     #Get active cehckouts
-
-
-
-
+    def get_active_checkouts (self):
+        return database.get_active_checkouts()
 
     #Get overdue Books
+    def get_overdue_books (self):
+        return database.get_overdue_books()
+    
+    # Load Books
+    def load_books(self):
+        all_books = database.get_all_books()
+        for data in all_books:
+            book = Book(data[0], data[1], data[2], data[3], data[4])
+            self.books.append(book)
+            self.books_by_id[book.book_id] = book
+        self.load_books
